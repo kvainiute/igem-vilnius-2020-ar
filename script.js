bb69ef577471be1422330641f95b9497dc8388f1
kvainiute <47873647+kvainiute@users.noreply.github.com>
2020-04-03 00:40:24 +0300

    dependencies for index.html

---
+++ script.js
@@ -0,0 +1,220 @@
+var camera, ambient, scene, renderer, video, update, model, models, raycaster, mouse, selectedObject, constraints;
+
+var strDownloadMime = "image/octet-stream";
+
+
+function init() {
+
+    //initial Media setup
+    var defaultsOpts = { audio: false, video: true };
+    var shouldFaceUser = false;
+    var stream = null;
+
+    //getting Users webcam input
+    function capture() {
+        defaultsOpts.video = { facingMode: shouldFaceUser ? 'user' : 'environment' };
+        navigator.mediaDevices.getUserMedia(defaultsOpts).then(function (streamTemp) {
+            stream  = streamTemp;
+            video.srcObject = stream;
+            video.play();
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+    capture();
+
+    //setting up the camera
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.z = 1.5;
+
+    //setting up the scene
+    scene = new THREE.Scene();
+    ambient = new THREE.AmbientLight(0xffffff, 1.0);
+    scene.add(ambient);
+
+    //setting up the renderer
+    renderer = new THREE.WebGLRenderer( { antialias: true, preserveDrawingBuffer: true} );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    var canvas = renderer.domElement;
+
+    //setting the background as webcam stream
+    video = document.getElementById( 'video' );
+    var webBackground = new THREE.VideoTexture( video );
+    scene.background = webBackground;
+
+    //camera controls
+    var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+    //choosing the 3D object
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2()
+
+    //All the event listeners
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    renderer.domElement.addEventListener( 'click', onClick, false );
+
+    document.getElementById("photo-button").addEventListener( 'click', saveAsImage, false );
+
+    document.getElementById("info-button").addEventListener( 'click', function(){
+        $('#model-info').css('display', 'block');
+        positionInfoDiv();
+        console.log("Success");
+    }, false );
+
+    document.getElementById("close-button").addEventListener( 'click', function(){
+        $('#model-info').css('display', 'none');
+    }, false );
+    //Reversing the camera <- needs fixing
+    document.getElementById("reverse-button").addEventListener('click', function(){
+        if( stream == null ) {console.log(stream);return} else {console.log(stream)};
+        // we need to flip, stop everything
+        stream.getTracks().forEach(t => {
+            t.stop();
+        });
+        // toggle / flip
+        shouldFaceUser = !shouldFaceUser;
+        console.log(shouldFaceUser);
+        capture();
+
+    });
+
+    //loading the 3D model
+    models = new THREE.Object3D();
+    var loader = new THREE.GLTFLoader();
+    loader.load('assets/scene.gltf', function (gltf) {
+        model = gltf.scene;
+
+        //Putting the model in the center and scaling it
+        var mroot = model;
+        var bbox = new THREE.Box3().setFromObject(mroot);
+        var cent = bbox.getCenter(new THREE.Vector3());
+        var size = bbox.getSize(new THREE.Vector3());
+
+        //Rescale the object to normalized space
+        var maxAxis = Math.max(size.x, size.y, size.z);
+        mroot.scale.multiplyScalar(1.0 / maxAxis);
+        bbox.setFromObject(mroot);
+        bbox.getCenter(cent);
+        bbox.getSize(size);
+        //Reposition to 0,halfY,0
+        mroot.position.copy(cent).multiplyScalar(-1);
+        mroot.position.y-= (size.y * 0.5);
+        models.add(mroot);
+    },
+                // called while loading is progressing
+                function ( xhr ) {
+
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+    },
+                // called when loading has errors
+                function ( error ) {
+        console.log( 'An error happened' );
+
+    });
+    scene.add(models);
+
+    //rotation of the 3D model
+    update = function(){
+        models.rotation.y += 0.01;
+        models.rotation.z += 0.01;
+    };
+}
+//Called when clicking on the 3d model => info div pops up
+function onClick() {
+
+    event.preventDefault();
+
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    raycaster.setFromCamera( mouse, camera );
+
+    var intersects = raycaster.intersectObjects( scene.children, true );
+
+    if ( intersects.length > 0 ) {
+        selectedObject = intersects[0].object;
+        // toggle html element
+        $('#model-info').css('display', 'block');
+        positionInfoDiv();
+        console.log("Success");
+
+    }
+
+}
+//positioning the Info Div in the middle
+function positionInfoDiv() {
+
+    var height = $("#model-info").height();
+    var width = $("#model-info").width();
+    console.log( height, width );
+    console.log($("#model-info h1").width());
+    if(width < $("#model-info h1").width()){
+        width = $("#model-info h1").width();
+    }
+    var left = ( window.innerWidth - width ) / 2;
+    var top = ( window.innerHeight - height ) / 2;
+
+    $('#model-info').css({ left: left, top: top });
+
+}
+//Window resizing
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    positionTip();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+//important
+function animate() {
+
+    requestAnimationFrame( animate );
+    update();
+    renderer.render( scene, camera );
+
+}
+//Custom image file name 
+function defaultFileName (ext) {
+    const str = `${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}${ext}`;
+    var name = str.replace(/\//g, '-').replace(/:/g, '.');
+    console.log(name);
+    return name;
+}
+//Saving the image <- needs fixing
+function saveAsImage() {
+    var imgData, imgNode;
+
+    try {
+        var strMime = "image/jpeg";
+        imgData = renderer.domElement.toDataURL(strMime);
+
+        saveFile(imgData.replace(strMime, strDownloadMime), defaultFileName(".jpg"));
+
+    } catch (e) {
+        console.log(e);
+        return;
+    }
+
+}
+//Capturing the image
+var saveFile = function (strData, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download === 'string') {
+        document.body.appendChild(link); //Firefox requires the link to be in the body
+        link.download = filename;
+        link.href = strData;
+        link.click();
+        document.body.removeChild(link); //remove the link when done
+    } else {
+        location.replace(uri);
+    }
+}
+init();
+animate();
+
