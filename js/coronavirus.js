3a55152ed080f74981ac861f2e254ca92452fcef
kamilev <kamile.vainiute@gmc.stud.vu.lt>
2020-04-06 18:04:25 +0300

    added preliminary map

--- js/coronavirus.js
+++ js/coronavirus.js
@@ -2,7 +2,7 @@ var header = "COVID-19";
 var paragraph = "Koronavirusai yra virusai, kurie cirkuliuoja tarp gyvūnų, tačiau žinoma, kad kai kurie iš jų sukelia infekcijas žmonėms. Sukėlę infekciją žmonėms, jie toliau gali būti perduoti nuo žmogaus žmogui. Koronavirusų infekcijos šaltinis gali būti daugybė gyvūnų. Pavyzdžiui, Artimųjų Rytų respiracinio sindromo koronaviruso (MERS-CoV) šaltinis buvo kupranugariai, o sunkaus ūmaus respiracinio sindromo (SŪRS) - civetės katės.";
 
 var models = new THREE.Object3D();
-var modelName = "models/1stModel/scene.gltf";
+var modelName = "models/coronavirus/scene.gltf";
 
 
 import { init, animate } from './script.js';
