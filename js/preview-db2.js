3d9f831c8fe4d14e76468c7cbbf33f65c15c1130
Sasnauskas Paulius <paulius.sasn@gmail.com>
2020-06-19 14:40:00 +0300

    add rna, dna, virus pattern data

--- js/preview-db2.js
+++ js/preview-db2.js
@@ -10,7 +10,7 @@ const data = [
         },
         model: {
             path: "./models/lego.glb",
-            pattern: "kanji",
+            pattern: "lego",
             pos: {x: 0, y: 0.1, z: 0},
             rot: {x: 0, y: -1.6, z: 0},
 			scale: 1,
@@ -18,12 +18,6 @@ const data = [
             looponce: true,
             animationType: 'an',
         },
-        onVisible: ()=>{
-            document.querySelector("body").style.backgroundImage = "url('images/bg/bg-dark.png')";
-        },
-        onHidden: ()=>{
-            
-        }
     },
     
     // -------------------- gfp --------------------
@@ -62,16 +56,16 @@ const data = [
         },
         model: {
             path: "./models/crisprcas9.glb",
-			pattern: "hiro",
+			pattern: "lego",
             pos: {
                 x: 0,
                 y: 0.5,
                 z: 0
             },
             rot: {
-                x: 0.3,
-                y: -1.85,
-                z: 0.5
+                x: 0,
+                y: 0,
+                z: 0
             },
 			scale: 0.07,
             animated: true,
@@ -79,9 +73,84 @@ const data = [
             animationType: 'an',
 
         },
-        onVisible: ()=>{
+    },
+    {
+        meta: {
+            nameLT: "Deoksiribonukleorūgštis (DNR)",
+            infoLT: "Tekstas - tekstas",
+            nameEN: "Deoxyribonucleic acid (DNA)",
+            infoEN: "...",
         },
-        onHidden: ()=>{
+        model: {
+            path: "./models/DNA.glb",
+            pattern: "dna",
+            pos: {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+            rot: {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+			scale: 0.1,
+            animated: true,
+            looponce: false,
+            animationType: 'an',
+        },
+    },
+    {
+        meta: {
+            nameLT: "Ribonukleino rūgštis (RNR)",
+            infoLT: "Tekstas - tekstas",
+            nameEN: "Ribonucleic acid (RNA)",
+            infoEN: "...",
+        },
+        model: {
+            path: "./models/rna/rna.gltf",
+            pattern: "rna",
+            pos: {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+            rot: {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+			scale: 1,
+            animated: false,
+            looponce: false,
+            animationType: 'n',
+        },
+    },
+    {
+        meta: {
+            nameLT: "SARS-CoV-2",
+            infoLT: "Koronavirusai yra virusai, kurie cirkuliuoja tarp gyvūnų, tačiau žinoma, kad kai kurie iš jų sukelia infekcijas žmonėms. Sukėlę infekciją žmonėms, jie toliau gali būti perduoti nuo žmogaus žmogui. Koronavirusų infekcijos šaltinis gali būti daugybė gyvūnų. Pavyzdžiui, Artimųjų Rytų respiracinio sindromo koronaviruso (MERS-CoV) šaltinis buvo kupranugariai, o sunkaus ūmaus respiracinio sindromo (SARS-CoV-1) – civetės katės.",
+            nameEN: "SARS-CoV-2",
+            infoEN: "...",
+        },
+        model: {
+            path: "./models/coronavirus.glb",
+            pattern: "virus",
+            pos: {
+                x: 0,
+                y: 0.3,
+                z: 0
+            },
+            rot: {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+			scale: 1,
+            animated: false,
+            looponce: true,
+            animationType: 'n',
+
         },
     },
 ];
